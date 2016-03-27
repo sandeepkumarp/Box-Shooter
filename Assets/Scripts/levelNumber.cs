6a257055fa60306ba9f454cb4e4f3bf5887edbe2
Sandeep <sandeep.pilakhnawal@gmail.com>
2016-03-27 21:17:44 +0530

    Project Ready

---
+++ Assets/Scripts/levelNumber.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class levelNumber : MonoBehaviour
+{
+
+    int levelNum;
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        levelNum = Application.loadedLevel+1;
+        text.text = "LEVEL: " + levelNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
